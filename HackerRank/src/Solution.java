0cd4a94a5949607f34b6a5eca15eecc7f0028857
Pranav Nakate <pranav.nakate@gmail.com>
2014-07-30 16:23:19 -0400

    still timeout

--- HackerRank/src/Solution.java
+++ HackerRank/src/Solution.java
@@ -15,36 +15,25 @@ public class Solution {
 			inputNumbers[i] = Integer.parseInt(input.charAt(i)+"");
 			
 		}
-		long sum = substrings(inputNumbers);
+		long sum = substringNew(inputNumbers);
 		System.out.println((long)(sum % (Math.pow(10, 9) + 7)));;
-	//	System.out.println("number is " + arrayNum(inputNumbers,0,inputNumbers.length-1));
 	}
 	
-	public static long substrings(int[] inputNumbers){
+	public static long substringNew(int[] inputNumbers){
 		
-		/*if(i==length || length==0)
-			return 0;
-		
-		return substrings(inputNumbers,i++,length) + substrings(inputNumbers, i, length-1);*/
 		long sum = 0;
-		for(int i=0;i<inputNumbers.length;i++){
-			for(int j=1;j+i <= inputNumbers.length; j++){
-				sum += arrayNum(inputNumbers, i, j);
+		int length = inputNumbers.length;
+		
+		for(int i=0;i<length;i++){
+			for(int j=length-i; j>0;j--){
+				if(inputNumbers[i]!=0)
+					sum =(long)(sum + inputNumbers[i]*Math.pow(10, j-1)*(i+1));
+				
 				sum = (long)(sum % (Math.pow(10, 9) + 7));
 			}
 		}
 		return sum;
 	}
 	
-	public static long arrayNum(int[] inputNumbers,int start,int length){
-		
-		long acc = 0;
-		for(int i=0; i < length; i++){
-			
-			acc = acc*10 + inputNumbers[i + start]; 
-			
-		}
-	//	System.out.println(acc);
-		return acc;
-	}
+	
 }
